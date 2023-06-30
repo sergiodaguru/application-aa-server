811f5b966d60fb07fde3de43a96ec936248bb6b3
sergiod <sergiod@us.ibm.com>
2023-06-30 15:38:49 -0400

    Server 15th commit: Added headers to Server Response

--- routes/message.js
+++ routes/message.js
@@ -3,7 +3,21 @@ var router = express.Router();
 
 router.get('/', function(req, res, next) {
     console.log(req.query.content);
-  res.render('message', { title: 'Express' });
+    let headers = new Headers();
+
+      headers.append('Content-Type', 'application/json');
+      headers.append('Accept', 'application/json');
+
+      headers.append('Access-Control-Allow-Origin', 'http://localhost:3000');
+      headers.append('Access-Control-Allow-Credentials', 'true');
+
+      headers.append('GET', 'POST', 'OPTIONS');
+
+res.writeHead(200, {
+  'Content-Length': body.length,
+  'Content-Type': 'text/plain',
+   'Access-Control-Allow-Origin': 'https://application-a-application-1a-client.cloud-refresh-ephem-42lwq-59ac7c5b6a0c144b63d34c29d8ad6aa7-0000.us-south.containers.appdomain.cloud'});
+  res.send('message', { title: 'Express' });
 });
 
 module.exports = router;

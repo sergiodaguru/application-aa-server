a9458651b95d20bb6bbcf31bbf13ca296723c804
sergiod <sergiod@us.ibm.com>
2023-06-30 17:27:35 -0400

    Server 19th commit: Used correct statement to enable CORS in the header

--- routes/message.js
+++ routes/message.js
@@ -5,7 +5,7 @@ router.get('/', function(req, res, next) {
   res.set('Access-Control-Allow-Origin', '*');
   console.log(req.query.content);
   //'Access-Control-Allow-Origin': 'https://application-a-application-1a-client.cloud-refresh-ephem-42lwq-59ac7c5b6a0c144b63d34c29d8ad6aa7-0000.us-south.containers.appdomain.cloud'});
-  res.status("200").send('message', { title: 'Express' });
+  res.status(200).send('{"test":"this is a test"}');
 });
 
 module.exports = router;
